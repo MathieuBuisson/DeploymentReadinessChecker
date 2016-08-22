8981dd4350e393e305f5e0173eedec8b675bcbef
MathieuBuisson <matthieu.buisson@gmail.com>
2016-08-22 13:39:05 +0200

    Add comment-based help and module manifest

--- DeploymentReadinessChecker.psm1
+++ DeploymentReadinessChecker.psm1
@@ -1,20 +1,37 @@
-﻿#Requires -Version 3
+﻿#Requires -Version 4
 #Requires -Modules 'Pester'
 
 Function Test-DeploymentReadiness {
 
 <#
 .SYNOPSIS
-    
+    Validates that one or more computers meet the prerequisites for a software deployment/upgrade.
 
 .DESCRIPTION
+    Validates that one or more computers meet the prerequisites for a software deployment/upgrade.
+    The list of computers to check is specified via the ComputerName parameter.
+    The deployment or upgrade prerequisites are specified in one or more Pester-based test scripts
+    located in the same directory as the module or in a sub-directory.
+
+    It generates a NUnit-style test results file for each computer and a summary report in HTML format.
+
+.PARAMETER ComputerName
+    To specify one or more computers against which the prerequisites checks will be performed.
+
+.PARAMETER Credential
+    To specify the credentials to connect remotely to the target computers.
+
+.PARAMETER OutputPath
+    To specify in which directory the output test results files and the summary report should be located.
+    If the directory doesn't exist, it will be created.
+    If not specified, the defaut output path is the current directory.
+
+.PARAMETER TestParameters
+    If the test script(s) used to validate the prerequisites take parameters, their names and values can be specified as a hashtable via this parameter.
+    Then, the function will pass these into the Script parameter of Invoke-Pester, when calling the test script(s).
     
-
-.PARAMETER ParamName
-
-.PARAMETER ParamName
-
 .EXAMPLE
+    Test-DeploymentReadiness -ComputerName (Get-Content .\Computers_List.txt) -Credential (Get-Credential) -OutputPath $env:USERPROFILE\Desktop\DeploymentReadinessReport
 
 .EXAMPLE
 
@@ -22,7 +39,7 @@ Function Test-DeploymentReadiness {
     Author : Mathieu Buisson
     
 .LINK
-
+    https://github.com/MathieuBuisson/DeploymentReadinessChecker
 #>
 
 [cmdletbinding()]
@@ -61,3 +78,5 @@ Function Test-DeploymentReadiness {
     End {
     }
 }
+New-Alias -Name 'tdr' -Value 'Test-DeploymentReadiness'
+Export-ModuleMember -Function 'Test-DeploymentReadiness' -Alias 'tdr'
